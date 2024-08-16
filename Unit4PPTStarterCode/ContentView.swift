fdb68f2ed83dac2a53e145db67dc0a29c775b456
Raymond Chen <ahrchen@gmail.com>
2024-08-16 10:15:31 -0700

    Using the same user interface from earlier lets add a state variable and a button to change the color of the text.

--- Unit4PPTStarterCode/ContentView.swift
+++ Unit4PPTStarterCode/ContentView.swift
@@ -8,13 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showFavoriteColor: Bool = false
+    
     var body: some View {
         VStack {
             Text("Hello, world!")
                 .font(.headline)
+                
             Text("Hello CodePath!!")
                 .font(.largeTitle)
         }
+        .foregroundStyle(showFavoriteColor ? .black : .blue)
+        
+        Button(action: {
+            showFavoriteColor.toggle()
+        }) {
+            Text("Show my favorite color")
+                .foregroundColor(.white)
+                .padding()
+                .frame(maxWidth: .infinity)
+        }
+        .background(Capsule().fill(Color.blue))
+        .padding(.horizontal)
+        
+        
         .padding()
     }
 }
