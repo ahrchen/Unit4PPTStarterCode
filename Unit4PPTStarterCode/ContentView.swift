d40de93e42368662b5224189d8c2b3a95873014a
Raymond Chen <ahrchen@gmail.com>
2024-08-16 09:57:20 -0700

    Initial Commit

---
+++ Unit4PPTStarterCode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Unit4PPTStarterCode
+//
+//  Created by Raymond Chen on 8/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Unit4PPTStarterCode/Unit4PPTStarterCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit4PPTStarterCodeApp.swift
+//  Unit4PPTStarterCode
+//
+//  Created by Raymond Chen on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Unit4PPTStarterCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
